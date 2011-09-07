cf944e06006188561e82a2f2b0ba0526bca0a5a5
Your Name <ts@ts-VPCEG1BFX.(none)>
2011-09-07 19:07:25 -0400

    commit about commit

--- lib/blog.rb
+++ lib/blog.rb
@@ -4,10 +4,20 @@
 
 class Blog
 
+def latest_title
+    page = Net::HTTP.get(URI.parse @url)
+    doc = Hpricot page 
+    xpath = "/html/body/div[2]/div/div[2]/div/div/h3"
+    element = doc.search xpath
+    element.inner_text
+end
+
+
 def initialize new_url
      @url = new_url 
 end
 end
 
 
+ 
  
